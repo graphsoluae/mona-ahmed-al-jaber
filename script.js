dbff3a7b7818afa2b3b83a7beb560a49de8fcf48
graphsoluae <128033359+graphsoluae@users.noreply.github.com>
2023-03-22 15:22:11 +0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ saveBtn.addEventListener("click", function () {
 
    
    
-    var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN;CHARSET=UTF-8:\nN: Al Jaber; Mona ; Ahmed ;\nTEL;TYPE=CELL:+971506362226\nEMAIL:maljaber@tawazun.ae\nORG:Tawazun council\nTITLE:Chief Corporate Communication\nURL: www.tawazun.ae\nADR; TYPE = WORK: ;;; Abudhabi;United Arab Emirates;;\nTITLE:\nREV:Wed Mar 22 2023 12: 55: 50 GMT + 0400(Gulf Standard Time)\nEND:VCARD";
+    var vcard = "BEGIN:VCARD\nVERSION:3.0\nFN;CHARSET=UTF-8:\nN: Al Jaber; Mona ; Ahmed ;\nTEL;TYPE=CELL:+971506362226\nEMAIL:maljaber@tawazun.ae\nORG:Tawazun council\nTITLE:Chief Corporate Communication\nURL: www.tawazun.ae\nADR; TYPE = WORK: ;;; Abudhabi;United Arab Emirates;;\nTITLE:\nREV:Wed Mar 22 2023 12: 55: 50 GMT + 0400(Gulf Standard Time)\nEND:VCARD";
 
     var blob = new Blob([vcard], { type: "text/vcard" });
     var url = URL.createObjectURL(blob);
